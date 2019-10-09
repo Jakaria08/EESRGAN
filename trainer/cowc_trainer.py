7413db5e1861eaad969a57cc0430587882659d74
jakaria08 <jakaria.rabbi@live.com>
2019-10-08 22:59:01 -0600

    training testing batch size ok

--- trainer/cowc_trainer.py
+++ trainer/cowc_trainer.py
@@ -43,8 +43,8 @@ class COWCTrainer(BaseTrainer):
         self.train_metrics.reset()
         for batch_idx, dataset_dict in enumerate(self.data_loader):
             print(dataset_dict['image'].size())
-            print(dataset_dict['bboxes'].size()
-            print(dataset_dict['labels'].size()
+            print(dataset_dict['bboxes'].size())
+            print(dataset_dict['labels'].size())
             print(dataset_dict['label_car_type'].size())
             print(dataset_dict['idx'].size())
         '''
