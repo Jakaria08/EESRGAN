becccf00adcee3abe400351b9ce62c900d4d055a
jakaria08 <jakaria.rabbi@live.com>
2019-10-10 22:56:03 -0600

    binary classification..

--- test_all/test_classes.py
+++ test_all/test_classes.py
@@ -7,14 +7,14 @@ from scripts_for_datasets import COWCDataset
 # python -m pytest test_all/
 class TestCOWCDataset():
 
-    config = read_json('config.json')
-    config = ConfigParser(config)
-    data_dir = config['data_loader']['args']['data_dir']
-    a = COWCDataset(data_dir)
-
     def test_image_annot_equality(self):
         # Test code for init method
         # Testing the dataset size and similarity
+        config = read_json('config.json')
+        config = ConfigParser(config)
+        data_dir = config['data_loader']['args']['data_dir']
+        os.rmdir("../saved")
+        a = COWCDataset(data_dir)
         for img, annot in zip(a.imgs, a.annotation):
             if os.path.splitext(img)[0] != os.path.splitext(annot)[0]:
                 print("problem")
@@ -23,6 +23,11 @@ class TestCOWCDataset():
 
     def test_zero_annotation(self):
         # Test for checking number of image without bounding box
+        config = read_json('config.json')
+        config = ConfigParser(config)
+        data_dir = config['data_loader']['args']['data_dir']
+        os.rmdir("../saved")
+        a = COWCDataset(data_dir)
         zero_annotation = 0
         for i in range(len(a.annotation)):
             _, zero_annotation_get =  a[i]
