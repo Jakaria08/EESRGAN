4ad602f05f487b1e6b4c8eb2a1f7b116c48bc09f
jakaria08 <jakaria.rabbi@live.com>
2019-10-28 17:37:24 -0600

    GAN Training Code start

--- trainer/cowc_GAN_trainer.py
+++ trainer/cowc_GAN_trainer.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+import math
+from scripts_for_datasets import COWCDataset, COWCGANDataset
 from torchvision.utils import make_grid
 from base import BaseTrainer
 from utils import inf_loop, MetricTracker, visualize_bbox, visualize
@@ -17,12 +19,18 @@ class COWCGANTrainer:
 
         self.valid_data_loader = valid_data_loader
         self.do_validation = self.valid_data_loader is not None
+        n_gpu = torch.cuda.device_count()
+        self.device = torch.device('cuda:0' if n_gpu > 0 else 'cpu')
+        self.train_size = int(math.ceil(self.data_loader.length / int(config['data_loader']['args']['batch_size'])))
+        self.total_iters = int(config['train']['niter'])
+        self.total_epochs = int(math.ceil(self.total_iters / self.train_size))
+        print(self.total_epochs)
 
     def train(self):
         '''
         Training logic for an epoch
         for visualization use the following code (use batch size = 1):
-        '''
+
         category_id_to_name = {1: 'car'}
         for batch_idx, dataset_dict in enumerate(self.data_loader):
             if dataset_dict['idx'][0] == 10:
@@ -30,3 +38,5 @@ class COWCGANTrainer:
                 visualize(dataset_dict, category_id_to_name) #--> see this method in util
 
         #image size: torch.Size([10, 3, 256, 256]) if batch_size = 10
+        '''
+        pass
