601c9bcabd0124bc7ffec9da65fbc8b17ba9f131
jakaria08 <jakaria.rabbi@live.com>
2019-10-27 22:42:25 -0600

    Dataoader for GAN........

--- trainer/cowc_GAN_trainer.py
+++ trainer/cowc_GAN_trainer.py
@@ -26,6 +26,7 @@ class COWCGANTrainer:
         '''
         category_id_to_name = {1: 'car'}
         for batch_idx, dataset_dict in enumerate(self.data_loader):
+            print(dataset_dict)['idx']
             if dataset_dict['idx'].squeeze() == 10:
                 print(dataset_dict)
                 visualize(dataset_dict, category_id_to_name) #--> see this method in util
