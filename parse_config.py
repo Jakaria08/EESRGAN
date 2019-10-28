65a107bf724be38fda0896da699182889dbfa261
jakaria08 <jakaria.rabbi@live.com>
2019-10-28 17:38:46 -0600

    GAN Training Code start.

--- parse_config.py
+++ parse_config.py
@@ -23,7 +23,7 @@ class ConfigParser:
         self.resume = resume
 
         # set save_dir where trained model and log will be saved.
-        save_dir = Path(self.config['trainer']['save_dir'])
+        save_dir = Path(self.config['train']['save_dir'])
 
         exper_name = self.config['name']
         if run_id is None: # use timestamp as default run-id
