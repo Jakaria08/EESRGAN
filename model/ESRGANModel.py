1ddf0f47e93f3f74679d3e2de68e18ca3e500b15
jakaria08 <jakaria.rabbi@live.com>
2019-10-28 20:43:34 -0600

    GAN Training Coding.....

--- model/ESRGANModel.py
+++ model/ESRGANModel.py
@@ -10,6 +10,8 @@ class ESRGANModel:
         self.configG = config['network_G']
         self.configD = config['network_D']
         self.configT = config['train']
+        self.configO = config['optimizer']['args']
+        self.configS = config['lr_scheduler']
         self.device = device
         self.netG = model.RRDBNet(in_nc=self.configG['in_nc'], out_nc=self.configG['out_nc'],
                                     nf=self.configG['nf'], nb=self.configG['nb'])
@@ -19,9 +21,9 @@ class ESRGANModel:
 
         self.netG.train()
         self.netD.train()
-        #print(float(self.configT['pixel_weight']))
+        print(self.configT['pixel_weight'])
         # G pixel loss
-        if float(self.configT['pixel_weight']) > 0.0:
+        if self.configT['pixel_weight'] > 0.0:
             l_pix_type = self.configT['pixel_criterion']
             if l_pix_type == 'l1':
                 self.cri_pix = nn.L1Loss().to(self.device)
@@ -54,7 +56,50 @@ class ESRGANModel:
 
         # GD gan loss
         self.cri_gan = GANLoss(self.configT['gan_type'], 1.0, 0.0).to(self.device)
-        self.l_gan_w = float(self.configT['gan_weight'])
+        self.l_gan_w = self.configT['gan_weight']
         # D_update_ratio and D_init_iters
         self.D_update_ratio = self.configT['D_update_ratio'] if self.configT['D_update_ratio'] else 1
         self.D_init_iters = self.configT['D_init_iters'] if self.configT['D_init_iters'] else 0
+
+
+        # optimizers
+        # G
+        wd_G = self.configO['weight_decay_G'] if self.configO['weight_decay_G'] else 0
+        optim_params = []
+        for k, v in self.netG.named_parameters():  # can optimize for a part of the model
+            if v.requires_grad:
+                optim_params.append(v)
+
+        self.optimizer_G = torch.optim.Adam(optim_params, lr=self.configO['lr_G'],
+                                            weight_decay=wd_G,
+                                            betas=(self.configO['beta1_G'], self.configO['beta2_G']))
+        self.optimizers.append(self.optimizer_G)
+        # D
+        wd_D = self.configO['weight_decay_D'] if self.configO['weight_decay_D'] else 0
+        self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=self.configO['lr_D'],
+                                            weight_decay=wd_D,
+                                            betas=(self.configO['beta1_D'], self.configO['beta2_D']))
+        self.optimizers.append(self.optimizer_D)
+
+        # schedulers
+        if self.configS['type'] == 'MultiStepLR':
+            for optimizer in self.optimizers:
+                self.schedulers.append(
+                    lr_scheduler.MultiStepLR_Restart(optimizer, self.configS['args']['lr_steps'],
+                                                     restarts=self.configS['args']['restarts'],
+                                                     weights=self.configS['args']['restart_weights'],
+                                                     gamma=self.configS['args']['lr_gamma'],
+                                                     clear_state=False))
+        elif train_opt['type'] == 'CosineAnnealingLR_Restart':
+            for optimizer in self.optimizers:
+                self.schedulers.append(
+                    lr_scheduler.CosineAnnealingLR_Restart(
+                        optimizer, self.configS['args']['T_period'], eta_min=self.configS['args']['eta_min'],
+                        restarts=self.configS['args']['restarts'], weights=self.configS['args']['restart_weights']))
+        else:
+            raise NotImplementedError('MultiStepLR learning rate scheme is enough.')
+        print(self.configS['args']['restarts'])
+        self.log_dict = OrderedDict()
+
+    #self.print_network()  # print network
+    #self.load()  # load G and D if needed
