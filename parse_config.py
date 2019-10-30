e639a0fb36da00c9d3e17e20894885192b1df5ce
jakaria08 <jakaria.rabbi@live.com>
2019-10-29 20:24:04 -0600

    GAN Training fixing bugs....

--- parse_config.py
+++ parse_config.py
@@ -40,7 +40,7 @@ class ConfigParser:
         write_json(self.config, self.save_dir / 'config.json')
         '''
         # configure logging module
-        setup_logging(self.log_dir)
+        #setup_logging(self.log_dir)
         self.log_levels = {
             0: logging.WARNING,
             1: logging.INFO,
