71fbff3491b429a673d7a2ea081f4bca503f17fd
jakaria08 <jakaria.rabbi@live.com>
2019-10-31 01:15:55 -0600

    ESRGAN Training working single GPU..

--- utils/util.py
+++ utils/util.py
@@ -350,16 +350,20 @@ def tensor2img(tensor, out_type=np.uint8, min_max=(0, 1)):
     Input: 4D(B,(3/1),H,W), 3D(C,H,W), or 2D(H,W), any range, RGB channel order
     Output: 3D(H,W,C) or 2D(H,W), [0,255], np.uint8 (default)
     '''
-    tensor = tensor.squeeze().float().cpu().clamp_(*min_max)  # clamp
-    tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])  # to range [0,1]
+    #tensor = tensor.squeeze().float().cpu().clamp_(*min_max)  # clamp
+    #tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])  # to range [0,1]
     n_dim = tensor.dim()
     if n_dim == 4:
         n_img = len(tensor)
         img_np = make_grid(tensor, nrow=int(math.sqrt(n_img)), normalize=False).numpy()
         img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
-    elif n_dim == 3:
-        img_np = tensor.numpy()
-        img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
+    elif n_dim == 3: # Now only works for dimension 3
+        img_np = tensor.squeeze().cpu().numpy().transpose(1,2,0).copy()
+        mean = np.array([0.3442, 0.3708, 0.3476])
+        std = np.array([0.1232, 0.1230, 0.1284])
+        img_np = std * img_np + mean
+        img_np = np.clip(img_np, 0, 1)
+        img_np = img_np*255
     elif n_dim == 2:
         img_np = tensor.numpy()
     else:
