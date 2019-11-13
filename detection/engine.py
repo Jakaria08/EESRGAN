48b1e5e346fc12bcde0f772ac3d6b6f944ec4b81
jakaria08 <jakaria.rabbi@live.com>
2019-11-12 19:35:29 -0700

    testing frcnn........

--- detection/engine.py
+++ detection/engine.py
@@ -5,9 +5,9 @@ import torch
 
 import torchvision.models.detection.mask_rcnn
 
-from coco_utils import get_coco_api_from_dataset
-from coco_eval import CocoEvaluator
-import utils
+from .coco_utils import get_coco_api_from_dataset
+from .coco_eval import CocoEvaluator
+import .utils as utils
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
