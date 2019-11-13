7062ddcdd8f4a4fe4f474d9a9874755f8d8c763a
jakaria08 <jakaria.rabbi@live.com>
2019-11-12 23:43:31 -0700

    testing frcnn running resolve...

--- detection/coco_eval.py
+++ detection/coco_eval.py
@@ -13,7 +13,7 @@ import pycocotools.mask as mask_util
 
 from collections import defaultdict
 
-import utils
+from .utils import all_gather
 
 
 class CocoEvaluator(object):
@@ -161,8 +161,8 @@ def convert_to_xywh(boxes):
 
 
 def merge(img_ids, eval_imgs):
-    all_img_ids = utils.all_gather(img_ids)
-    all_eval_imgs = utils.all_gather(eval_imgs)
+    all_img_ids = all_gather(img_ids)
+    all_eval_imgs = all_gather(eval_imgs)
 
     merged_img_ids = []
     for p in all_img_ids:
