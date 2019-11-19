69902ba21a5d574d386d6b31b3d34a07fc4e0aff
jakaria08 <jakaria.rabbi@live.com>
2019-11-18 20:25:26 -0700

    Test FRCNN........

--- trainer/FRCNN_trainer.py
+++ trainer/FRCNN_trainer.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import numpy as np
 import torchvision
 import os
-from torch.nn.parallel import DataParallel
+from torch.nn.parallel import DataParallel, DistributedDataParallel
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torch.utils.data import Dataset, DataLoader
 from torchvision import utils
