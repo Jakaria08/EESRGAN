5c2f613ee46d46bf7c300d4042443be4ddb44647
jakaria08 <jakaria.rabbi@live.com>
2019-11-25 18:27:01 -0700

    Combined EEGAN FRCNN coding......

--- scripts_for_datasets/COWC_EESRGAN_FRCNN_dataset.py
+++ scripts_for_datasets/COWC_EESRGAN_FRCNN_dataset.py
@@ -79,6 +79,7 @@ class COWCGANFrcnnDataset(Dataset):
 
     if obj_class != 0:
         labels = np.ones(len(boxes)) # all are cars
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         iscrowd = torch.zeros((len(boxes),), dtype=torch.int64)
         #create dictionary to access the values
