c35e8a67744fea89f08c469387fd6ac81de902a0
jakaria08 <jakaria.rabbi@live.com>
2019-11-26 20:23:38 -0700

    Combined EEGAN FRCNN tying to run, fixing errors......

--- model/ESRGAN_EESN_FRCNN_Model.py
+++ model/ESRGAN_EESN_FRCNN_Model.py
@@ -223,7 +223,7 @@ class ESRGAN_EESN_FRCNN_Model(BaseModel):
         self.optimizer_FRCNN.zero_grad()
         self.intermediate_img = self.fake_H.detach()
         img_count = self.intermediate_img.size()[0]
-        self.intermediate_img = [self.intermediate_img[i] for i in img_count]
+        self.intermediate_img = [self.intermediate_img[i] for i in range(img_count)]
         loss_dict = self.netFRCNN(self.intermediate_img, self.targets)
         losses = sum(loss for loss in loss_dict.values())
 
