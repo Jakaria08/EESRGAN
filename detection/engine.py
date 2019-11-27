24d6f990dd8880ae1640bc7bce22a8f780149e3c
jakaria08 <jakaria.rabbi@live.com>
2019-11-26 19:25:06 -0700

    Combined EEGAN FRCNN tying to run, fixing errors..

--- detection/engine.py
+++ detection/engine.py
@@ -26,7 +26,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        print(images.size())
+        print(images)
         loss_dict = model(images, targets)
 
         losses = sum(loss for loss in loss_dict.values())
