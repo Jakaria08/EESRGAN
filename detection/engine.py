3b500ea8811330f2db6eb3e30f1692490c14ed69
jakaria08 <jakaria.rabbi@live.com>
2019-12-09 20:33:14 -0700

    train FRCNN witth 5 class first..EESRGAN_FRCNN with multi class cowc - dataset changed...

--- detection/engine.py
+++ detection/engine.py
@@ -27,7 +27,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         print(targets)
-        '''
+        
         loss_dict = model(images, targets)
 
         losses = sum(loss for loss in loss_dict.values())
@@ -52,7 +52,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-        '''
+
 
 def _get_iou_types(model):
     model_without_ddp = model
