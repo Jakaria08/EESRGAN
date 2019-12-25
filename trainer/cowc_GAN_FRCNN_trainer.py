c321126adbc8b9c8b83bc3b94af269e58abaa8f0
jakaria08 <jakaria.rabbi@live.com>
2019-12-24 18:30:40 -0700

    classify with edge enhanced images

--- trainer/cowc_GAN_FRCNN_trainer.py
+++ trainer/cowc_GAN_FRCNN_trainer.py
@@ -72,8 +72,8 @@ class COWCGANFrcnnTrainer:
                 from tensorboardX import SummaryWriter
             tb_logger = SummaryWriter(log_dir='saved/tb_logger/' + self.config['name'])
 
-        current_step = 16001
-        start_epoch = 43
+        current_step = 0
+        start_epoch = 0
 
         #### training
         logger.info('Start training from epoch: {:d}, iter: {:d}'.format(start_epoch, current_step))
