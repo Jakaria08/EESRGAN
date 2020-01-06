400ace902d994ffc0814481bcb98d0ae9c875ab1
jakaria08 <jakaria.rabbi@live.com>
2020-01-06 00:46:39 -0700

    HR FRCNN with xview dataset 21 classes

--- trainer/FRCNN_trainer.py
+++ trainer/FRCNN_trainer.py
@@ -168,7 +168,8 @@ class COWCFRCNNTrainer:
     def train(self):
         # load a model pre-trained pre-trained on COCO
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn()
-
+        model.to(self.device)
+        self.load_model(self.config['path']['pretrain_model_FRCNN'], model)
         # replace the classifier with a new one, that has
         # num_classes which is user-defined
         num_classes = 21  # 1 class (car) + background
@@ -176,9 +177,6 @@ class COWCFRCNNTrainer:
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         # replace the pre-trained head with a new one
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-        model.to(self.device)
-        self.load_model(self.config['path']['pretrain_model_FRCNN'], model)
 
         # construct an optimizer
         params = [p for p in model.parameters() if p.requires_grad]
