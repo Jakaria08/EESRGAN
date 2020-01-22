all fix for xview train not backpropagated

scripts_for_datasets/COWC_EESRGAN_FRCNN_dataset.py
@@ -55,7 +55,7 @@ class COWCGANFrcnnDataset(Dataset):
                 target['image_lq'] = img_lq
                 target['image'] = img_gt
                 target['bboxes'] = boxes
-                target['labels'] = labels
+                target['labels'] = label_car_type
                 target['label_car_type'] = label_car_type
                 target['image_id'] = idx
                 target['LQ_path'] = img_path_lq
@@ -64,15 +64,20 @@ class COWCGANFrcnnDataset(Dataset):
                 break
             else:
                 #get coordinates withing height width range
+                '''
                 x = float(values[1])*self.image_width
                 y = float(values[2])*self.image_height
                 width = float(values[3])*self.image_width
                 height = float(values[4])*self.image_height
+                '''
                 #creating bounding boxes that would not touch the image edges
-                x_min = 1 if x - width/2 <= 0 else int(x - width/2)
-                x_max = 255 if x + width/2 >= 256 else int(x + width/2)
-                y_min = 1 if y - height/2 <= 0 else int(y - height/2)
-                y_max = 255 if y + height/2 >= 256 else int(y + height/2)
+                x_min = 1 if int(values[1]) <= 0 else int(values[1])
+                y_min = 1 if int(values[2]) <= 0 else int(values[2])
+                x_max = 255 if int(values[3]) >= 256 else int(values[3])
+                y_max = 255 if int(values[4]) >= 256 else int(values[4])
+
+                if x_max - x_min < 3 or y_max - y_min < 3:
+                    continue
 
                 boxes.append([x_min, y_min, x_max, y_max])
                 label_car_type.append(obj_class)
@@ -88,7 +93,7 @@ class COWCGANFrcnnDataset(Dataset):
         target['image_lq'] = img_lq
         target['image'] = img_gt
         target['bboxes'] = boxes
-        target['labels'] = labels
+        target['labels'] = label_car_type
         target['label_car_type'] = label_car_type
         target['image_id'] = idx
         target['LQ_path'] = img_path_lq
@@ -115,7 +120,7 @@ class COWCGANFrcnnDataset(Dataset):
       target['image_lq'] = torch.from_numpy(target['image_lq'].transpose((2, 0, 1)))
       target['image'] = torch.from_numpy(target['image'].transpose((2, 0, 1)))
       target['boxes'] = torch.tensor(target['bboxes'], dtype=torch.float32)
-      target['labels'] = torch.ones(len(target['labels']), dtype=torch.int64)
+      target['labels'] = torch.tensor(target['label_car_type'], dtype=torch.int64)
       target['label_car_type'] = torch.tensor(target['label_car_type'], dtype=torch.int64)
       target['image_id'] = torch.tensor([target['image_id']])
       target["area"] = torch.tensor(target['area'])

scripts_for_datasets/cowc_FRCNN_dataset.py
@@ -27,11 +27,14 @@ class COWCFRCNNDataset(Dataset):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #get the bounding box
     boxes = list()
+    label_type = list()
     with open(annotation_path) as f:
         for line in f:
             values = (line.split())
             if "\ufeff" in values[0]:
               values[0] = values[0][-1]
+            obj_class = int(values[0])
+            label_type.append(obj_class)
             #get coordinates withing height width range
             '''
             x = float(values[1])*self.image_width
@@ -53,7 +56,7 @@ class COWCFRCNNDataset(Dataset):
 
     boxes = torch.as_tensor(boxes, dtype=torch.float32)
     # there is only one class
-    labels = torch.ones((len(boxes),), dtype=torch.int64)
+
     image_id = torch.tensor([idx])
     area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
     # suppose all instances are not crowd
@@ -61,7 +64,7 @@ class COWCFRCNNDataset(Dataset):
     #create dictionary to access the values
     target = {}
     target["boxes"] = boxes
-    target["labels"] = labels
+    target['labels'] = torch.tensor(label_type, dtype=torch.int64)
     target["image_id"] = image_id
     target["area"] = area
     target["iscrowd"] = iscrowd