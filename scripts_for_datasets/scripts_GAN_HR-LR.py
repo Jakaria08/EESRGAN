9534ab81ff2a7f3c65f239db5c40862485a6306f
jakaria08 <jakaria.rabbi@live.com>
2020-02-09 21:25:28 -0700

    create txt file

--- scripts_for_datasets/scripts_GAN_HR-LR.py
+++ scripts_for_datasets/scripts_GAN_HR-LR.py
@@ -422,8 +422,8 @@ def xml_to_text():
                             count = count + 1
                     if subelem.tag == 'bndbox':
                         for subsubelem in subelem:
-                            coords.append(subsubelem.text)
-                        class_box.append(['1', coords[0], coords[1], coords[2], coords[3]])
+                            coords.append(int(subsubelem.text))
+                        class_box.append([1, coords[0], coords[1], coords[2], coords[3]])
 
         cls_box = np.matrix(class_box)
 
