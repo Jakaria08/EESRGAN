f729650ddb37c5daf946c111140cedb2e011931e
jakaria08 <jakaria.rabbi@live.com>
2020-02-10 00:23:58 -0700

    FRCNN tank dataset

--- trainer/FRCNN_trainer.py
+++ trainer/FRCNN_trainer.py
@@ -201,6 +201,6 @@ class COWCFRCNNTrainer:
             # update the learning rate
             lr_scheduler.step()
             # evaluate on the test dataset
-            evaluate_base(model, data_loader_test_Bic, device=self.device)
+            evaluate_base(model, data_loader_test, device=self.device)
             if epoch % 10 == 0:
                 self.save_model(model, 'FRCNN_HR_tank', epoch)
