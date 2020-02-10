206175dbbe0a7c29a8e01c6949e67b310aa50eee
jakaria08 <jakaria.rabbi@live.com>
2020-02-10 00:16:33 -0700

    FRCNN tank dataset

--- scripts_for_datasets/cowc_FRCNN_dataset.py
+++ scripts_for_datasets/cowc_FRCNN_dataset.py
@@ -7,16 +7,14 @@ import cv2
 from torch.utils.data import Dataset, DataLoader
 
 class COWCFRCNNDataset(Dataset):
-  def __init__(self, root=None, image_height=256, image_width=256, transforms = None):
+  def __init__(self, root=None, image_height=512, image_width=512, transforms = None):
     self.root = root
     #take all under same folder for train and test split.
     self.transforms = transforms
     self.image_height = image_height
     self.image_width = image_width
     #sort all images for indexing, filter out check.jpgs
-    self.imgs = list(sorted(set(glob.glob(self.root+"*.jpg") +
-                    glob.glob(self.root+"*.png")) -
-                    set(glob.glob(self.root+"*check.jpg"))))
+    self.imgs = list(sorted(glob.glob(self.root+"*.png")))
     self.annotation = list(sorted(glob.glob(self.root+"*.txt")))
 
   def __getitem__(self, idx):
@@ -32,16 +30,19 @@ class COWCFRCNNDataset(Dataset):
             values = (line.split())
             if "\ufeff" in values[0]:
               values[0] = values[0][-1]
+              
+            '''
             #get coordinates withing height width range
             x = float(values[1])*self.image_width
             y = float(values[2])*self.image_height
             width = float(values[3])*self.image_width
             height = float(values[4])*self.image_height
+            '''
             #creating bounding boxes that would not touch the image edges
-            x_min = 1 if x - width/2 <= 0 else int(x - width/2)
-            x_max = 255 if x + width/2 >= 256 else int(x + width/2)
-            y_min = 1 if y - height/2 <= 0 else int(y - height/2)
-            y_max = 255 if y + height/2 >= 256 else int(y + height/2)
+            x_min = 1 if values[1] <= 0 else int(values[1])
+            y_min = 1 if values[2] <= 0 else int(values[2])
+            x_max = 511 if values[3] >= 512 else int(values[3])
+            y_max = 511 if values[4] >= 512 else int(values[4])
 
             boxes.append([x_min, y_min, x_max, y_max])
 
