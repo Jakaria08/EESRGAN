dc5375901fc6e5e4af6a5f3780966d3dc4e1407c
jakaria08 <jakaria.rabbi@live.com>
2020-02-10 19:56:27 -0700

    tank dataset EESRGAN-FRCNN train validation

--- train.py
+++ train.py
@@ -41,8 +41,8 @@ def main(config):
     #change later this valid_data_loader using init_obj
     #valid_data_loader = module_data.COWCGANFrcnnDataLoader('/home/jakaria/Super_Resolution/Datasets/TankData/HR_LR_BIC_Data/HR/x4/valid_img/',
     #'/home/jakaria/Super_Resolution/Datasets/TankData/HR_LR_BIC_Data/LR/x4/valid_img/', 1, training = False)
-    valid_data_loader = module_data.COWCGANFrcnnDataLoader('/home/jakaria/Super_Resolution/Datasets/COWC/DetectionPatches_256x256/Potsdam_ISPRS/HR/x4/valid_img/',
-    '/home/jakaria/Super_Resolution/Datasets/COWC/DetectionPatches_256x256/Potsdam_ISPRS/LR/x4/valid_img/', 1, training = False)
+    valid_data_loader = module_data.COWCGANFrcnnDataLoader('/home/jakaria/Super_Resolution/Datasets/TankData/HR_LR_BIC_Data/HR/x4/valid_img/',
+    '/home/jakaria/Super_Resolution/Datasets/TankData/HR_LR_BIC_Data/LR/x4/valid_img/', 1, training = False)
     # build model architecture, then print to console
     #model = config.init_obj('arch', module_arch)
     #logger.info(model)
