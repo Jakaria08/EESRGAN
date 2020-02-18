d7307c6979631c4a30e63364867c664e4dcb9ce3
jakaria08 <jakaria.rabbi@live.com>
2020-02-17 23:20:25 -0700

    testing detection results and generate bbox output with class confidence

--- detection/engine.py
+++ detection/engine.py
@@ -79,7 +79,7 @@ def get_prediction(outputs, file_path, threshold=0.5):
         new_class_conf_box.append([pred_class[i], pred_score[i], int(text_boxes[i][0]), int(text_boxes[i][1]), int(text_boxes[i][2]), int(text_boxes[i][3])])
     new_class_conf_box = np.matrix(new_class_conf_box)
 
-    np.savetxt(file_path, new_class_conf_box, fmt='%i')
+    np.savetxt(file_path, new_class_conf_box)
 
 '''
 for generating test boxes
