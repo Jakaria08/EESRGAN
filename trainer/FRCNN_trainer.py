67d6097906997531fd043a79f1af2e9c23c7143c
jakaria08 <jakaria.rabbi@live.com>
2020-02-17 19:07:11 -0700

    testing detection results and generate bbox output with class confidence

--- trainer/FRCNN_trainer.py
+++ trainer/FRCNN_trainer.py
@@ -129,7 +129,7 @@ class COWCFRCNNTrainer:
         pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())] # Bounding boxes
         text_boxes = [i for i in list(pred[0]['boxes'].detach().cpu().numpy())] # Bounding boxes
         pred_score = list(pred[0]['scores'].detach().cpu().numpy())
-        print(pred)
+        print(img)
         for i in range(len(text_boxes)):
             new_class_conf_box.append([int(pred_class[i]), int(pred_score[i][0]), int(text_boxes[i][0]), int(text_boxes[i][1]), int(text_boxes[i][2]), int(text_boxes[i][3])])
         new_class_conf_box = np.matrix(new_class_conf_box)
