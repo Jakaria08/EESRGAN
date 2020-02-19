55e5234023568df35a224b323c4127f56c2b9600
jakaria08 <jakaria.rabbi@live.com>
2020-02-18 17:35:44 -0700

    SR-SR tank test result

--- detection/engine.py
+++ detection/engine.py
@@ -83,7 +83,7 @@ def get_prediction(outputs, file_path, threshold=0.5):
 
         np.savetxt(file_path, new_class_conf_box, fmt="%i %1.3f %i %i %i %i")
     else:
-        np.savetxt(file_path, [0, 0, 0, 0, 0], fmt="%i %1.3f %i %i %i %i")
+        np.savetxt(file_path, [0, 0, 0, 0, 0, 0], fmt="%i %1.3f %i %i %i %i")
 
 '''
 for generating test boxes
