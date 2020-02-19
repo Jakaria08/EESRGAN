9bcebf8a846056abbebd64516c7d149906f56591
jakaria08 <jakaria.rabbi@live.com>
2020-02-18 19:42:10 -0700

    LR-LR car  test result

--- trainer/FRCNN_trainer.py
+++ trainer/FRCNN_trainer.py
@@ -135,7 +135,7 @@ class COWCFRCNNTrainer:
         pred_score = list(outputs[0]['scores'].detach().cpu().numpy())
         #print(pred_score)
         for i in range(len(text_boxes)):
-            new_class_conf_box.append([pred_class[i], pred_score[i], int(text_boxes[i][0]), int(text_boxes[i][1]), int(text_boxes[i][2]), int(text_boxes[i][3])])
+            new_class_conf_box.append([pred_class[i], pred_score[i], int(text_boxes[i][0]*4), int(text_boxes[i][1]*4), int(text_boxes[i][2]*4), int(text_boxes[i][3]*4)])
         new_class_conf_box = np.matrix(new_class_conf_box)
 
         np.savetxt(file_path, new_class_conf_box, fmt="%i %1.3f %i %i %i %i")
