e9b52898c4bbc2eb8ca1d8d10cc489a656e1bd8e
jakaria08 <jakaria.rabbi@live.com>
2020-02-25 16:17:56 -0700

    ssd train

--- model/ESRGAN_EESN_SSD_Model.py
+++ model/ESRGAN_EESN_SSD_Model.py
@@ -257,7 +257,7 @@ class ESRGAN_EESN_SSD_Model(BaseModel):
         losses = sum(loss for loss in loss_dict.values())
 
         # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = reduce_dict(loss_dict)
+        loss_dict_reduced = reduce_loss_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
         loss_value = losses_reduced.item()
@@ -311,6 +311,30 @@ class ESRGAN_EESN_SSD_Model(BaseModel):
         if need_GT:
             out_dict['GT'] = self.var_H.detach()[0].float().cpu()
         return out_dict
+
+    def reduce_loss_dict(loss_dict):
+    """
+    Reduce the loss dictionary from all processes so that process with rank
+    0 has the averaged results. Returns a dict with the same fields as
+    loss_dict, after reduction.
+    """
+    world_size = dist_util.get_world_size()
+    if world_size < 2:
+        return loss_dict
+    with torch.no_grad():
+        loss_names = []
+        all_losses = []
+        for k in sorted(loss_dict.keys()):
+            loss_names.append(k)
+            all_losses.append(loss_dict[k])
+        all_losses = torch.stack(all_losses, dim=0)
+        dist.reduce(all_losses, dst=0)
+        if dist.get_rank() == 0:
+            # only main process gets accumulated, so only divide by
+            # world_size in this case
+            all_losses /= world_size
+        reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
+    return reduced_losses
 
     def print_network(self):
         # Generator
